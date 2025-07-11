a9abd86895b61c97cf9ffe8cfc4db7007c2f2f19
keko1717 <s.cadizgajardo@gmail.com>
2025-07-11 02:45:19 +0000

    final proyect

--- src/app.js
+++ src/app.js
@@ -1,11 +1,23 @@
-import "bootstrap";
-import "./style.css";
+let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+let action = ['ate', 'peed on', 'crushed', 'broke'];
+let what = ['my homework', 'my phone', 'the car', 'my sandwich'];
+let when = [
+  'before the class',
+  'when I was sleeping',
+  'while I was exercising',
+  'during my lunch',
+  'while I was praying'
+];
 
+function generarExcusa() {
+  let parte1 = who[Math.floor(Math.random() * who.length)];
+  let parte2 = action[Math.floor(Math.random() * action.length)];
+  let parte3 = what[Math.floor(Math.random() * what.length)];
+  let parte4 = when[Math.floor(Math.random() * when.length)];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+  return `${parte1} ${parte2} ${parte3} ${parte4}.`;
+}
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  document.getElementById("excuse").innerHTML = generarExcusa();
 };
